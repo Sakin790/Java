915b7b0a5e7905c4186a3a9a30b0bb5776eac86c
Sakin790 <sakinislam79@gmail.com>
2024-02-04 23:04:17 +0600

    Variable

---
+++ Basic Java/variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args) {
+        String name = "Sakin";
+        int number = 123;
+        float floteNumber = 112f;
+        char alphabet = 'a';
+        boolean isActive = true;
+
+    }
+}
