edc39c92db3d745293a73cbaa9f8e1017665e71e
Sakin790 <sakinislam79@gmail.com>
2024-02-04 23:35:14 +0600

    added

--- Basic Java/variable.java
+++ Basic Java/variable.java
@@ -7,4 +7,5 @@ public class variable {
         boolean isActive = true;
 
     }
+
 }
---
+++ Basic Java/widening.java
@@ -0,0 +1,7 @@
+public class widening {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDuble = myInt;
+        System.out.println(myDuble);
+    }
+}
