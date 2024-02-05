febbbd518073f0da0bc25c4271f539394b6b153b
Sakin790 <sakinislam79@gmail.com>
2024-02-05 19:10:08 +0600

    String

---
+++ Basic Java/String.java
@@ -0,0 +1,8 @@
+public class String {
+    public static void main(String[] args) {
+      String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      System.out.println("The length of the txt string is: " + txt.length());
+    }
+  }
+
+  // As like javascript Everything is Same
