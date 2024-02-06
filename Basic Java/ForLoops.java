efd47ac0501d8219fb77017e42fc64e7b4785725
Sakin790 <sakinislam79@gmail.com>
2024-02-06 08:59:38 +0600

    might have bug

--- Basic Java/ForLoops.java
+++ Basic Java/ForLoops.java
@@ -1,5 +1,5 @@
-public class Main {
-    public  static void main(String[] args) {
+public class ForLoops {
+    public static void main(String[] args) {
         int i = 0;
         for(i = 0; i <= 10; i = i + 2) {
             System.out.println(i);
