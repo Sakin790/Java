0aa98cabb40b7f2aab00337628faf72397d4d13e
Sakin790 <sakinislam79@gmail.com>
2024-02-08 23:02:35 +0600

    Scanner

--- Advance/Main.java
+++ Advance/Main.java
@@ -1,6 +1,10 @@
 package Advance;
 
+import java.util.Scanner;
 
 public class Main {
-    
+    public static void main(String[] args) {
+       
+    }
+
 }
---
+++ Advance/Scanner.java
@@ -0,0 +1,15 @@
+package Advance;
+
+import java.util.Scanner;
+
+public class Scanner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int height = input.nextInt();
+        int weight = input.nextInt();
+
+        int sum = height + weight;
+        System.out.println(sum);
+
+    }
+}
