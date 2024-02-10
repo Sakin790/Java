e5a925e77c354f24b6c967906d6c96543cf86c56
Sakin790 <sakinislam79@gmail.com>
2024-02-10 12:08:31 +0600

    Fix some bug

--- Advance/Scanner_input.java
+++ Advance/Scanner_input.java
@@ -2,7 +2,7 @@ package Advance;
 
 import java.util.Scanner;
 
-public class Scanner {
+public class Scanner_input {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int height = input.nextInt();
---
+++ Advance/switchs.java
@@ -0,0 +1,22 @@
+package Advance;
+
+import java.util.Scanner;
+
+public class switchs {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int Day = in.nextInt();
+
+        switch (Day) {
+            case 1:
+                System.out.println("Day 1");
+                break;
+            case 2:
+                System.out.println("Day 2");
+                break;
+            default:
+            System.out.println("Please inter a number:");
+                break;
+        }
+    }
+}
