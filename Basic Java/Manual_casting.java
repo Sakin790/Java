537a9da77ff896bcb8052cec56dc52de6ea31638
sakin_dev@79 <107269051+Sakin790@users.noreply.github.com>
2024-02-13 21:11:27 +0600

    Update Manual_casting.java

--- Basic Java/Manual_casting.java
+++ Basic Java/Manual_casting.java
@@ -1,7 +1,7 @@
 public class Manual_casting {
     public static void main(String[] args) {
       double myDouble = 9.78d;
-      int myInt = (int) myDouble; // Manual casting: double to int
+      int myInt = (int) myDouble; 
   
       System.out.println(myDouble);   // Outputs 9.78
       System.out.println(myInt);      // Outputs 9
