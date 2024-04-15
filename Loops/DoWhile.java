d2b840d04ce61aedbc1cafe5b04ec411e51145f1
Sakin790 <sakinislam79@gmail.com>
2024-04-15 15:14:57 +0600

    x

---
+++ Loops/DoWhile.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i + " " + "Hello Wrold");
+            i++; 
+        } while (i <= 5);
+    }
+}
