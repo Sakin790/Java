7fba39aa3800aacfc754ac8e98ac2528ecdad1da
Sakin790 <sakinislam79@gmail.com>
2024-04-20 00:39:14 +0600

    Scanner

---
+++ CMD_Input/Input.java
@@ -0,0 +1,13 @@
+package CMD_Input;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Enter a String Valus...");
+        Boolean name = Input.nextBoolean();
+        System.out.println("String value is" + name);
+
+    }
+}
