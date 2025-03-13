e1d38a8ec3f88d9af11c75ec3241ea308431304f
Sakin790 <sakinislam79@gmail.com>
2025-03-14 00:44:03 +0600

    complete hashmap

---
+++ MyMap.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+
+public class MyMap {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        System.out.println(map);
+    }
+}
---
+++ YourMap.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class YourMap {
+    public static void main(String[] args) {
+        System.out.println("Leaarning HashMap in Java");
+        HashMap<String, Integer>myMap = new HashMap<>();
+        myMap.put("Sakin", 23);
+        myMap.put("mahid", 22);
+        System.out.println(myMap);
+
+    }
+}
